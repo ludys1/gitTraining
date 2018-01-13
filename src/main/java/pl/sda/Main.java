b054964b7c73b3c5ec6557acb234d08aa5f094f1
Filip Ludyga <f.ludyga@gmail.com>
2018-01-13 12:07:17 +0100

    Sto lat

--- src/main/java/pl/sda/Main.java
+++ src/main/java/pl/sda/Main.java
@@ -3,7 +3,7 @@ package pl.sda;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        System.out.println("Sto lat KOT!!!!");
     }
 
 }
