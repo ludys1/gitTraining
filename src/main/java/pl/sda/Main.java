ec75644dbc9a67a9a743fc0ff6e2fb2d25e0afbf
Filip Ludyga <f.ludyga@gmail.com>
2018-01-13 12:20:06 +0100

    cos

--- src/main/java/pl/sda/Main.java
+++ src/main/java/pl/sda/Main.java
@@ -5,6 +5,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Sto lat KOT!!!!");
         System.out.println("obanck cos");
+
+        System.out.println("dobra wpisuje kolejny tekst");
     }
 
 }
